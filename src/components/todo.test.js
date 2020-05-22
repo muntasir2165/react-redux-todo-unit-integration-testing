67115054b9b5707ada41eaec737f1613a482800f
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-22 09:33:14 -0700

    v-7 installing enzyme testing utility

--- src/components/todo.test.js
+++ src/components/todo.test.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import Todo from './Todo';
+import { configure, shallow, mount } from 'enzyme';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
