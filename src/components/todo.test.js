66401d170d24698b87dbfc552f828252049a7ee2
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-23 11:15:43 -0700

    v-8 first unit test for react component

--- src/components/todo.test.js
+++ src/components/todo.test.js
@@ -6,3 +6,18 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Todo /> component Unit Tests', () => {
+  const mockFn = jest.fn();
+  const props = {
+    onClick: mockFn,
+    completed: false,
+    text: 'buy milk',
+  };
+
+  it('should render 1 <Todo /> component', () => {
+    const component = shallow(<Todo {...props} />);
+    expect(component).toHaveLength(1);
+    expect(component.find('li')).toHaveLength(1);
+  });
+});
