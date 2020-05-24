def9c8ebc6c32b59983e9643b10b8db720a9a459
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-24 05:32:02 -0700

    v-11 setup and tear down

--- src/components/todo.test.js
+++ src/components/todo.test.js
@@ -14,27 +14,28 @@ describe('<Todo /> component Unit Tests', () => {
     completed: false,
     text: 'buy milk',
   };
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<Todo {...props} />);
+  });
 
   it('should render 1 <Todo /> component', () => {
-    const component = shallow(<Todo {...props} />);
     expect(component).toHaveLength(1);
     expect(component.find('li')).toHaveLength(1);
   });
 
   it('should render props correctly', () => {
-    const component = shallow(<Todo {...props} />);
     // console.log(component.props());
     expect(component.props().children).toEqual('buy milk');
   });
 
   it('should set props correctly', () => {
-    const component = shallow(<Todo {...props} />);
     component.setProps({ text: 'hello' });
     expect(component.props().children).toEqual('hello');
   });
 
   it('should call onClick handler when Todo component is clicked', () => {
-    const component = shallow(<Todo {...props} />);
     component.simulate('click');
     expect(mockFn).toHaveBeenCalledTimes(1);
   });
